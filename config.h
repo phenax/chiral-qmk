fe6398cc051f0871414d3cc0b2155f642169f3b1
Akshay Nair <phenax5@gmail.com>
2026-04-15 12:34:33 +0530

    Fix key layout and layout coloring

--- config.h
+++ config.h
@@ -5,3 +5,20 @@
 #define BOOTMAGIC_ROW 0
 #define BOOTMAGIC_COLUMN 0
 
+#ifndef DefaultLayerColorHSV
+#define DefaultLayerColorHSV 0xaa, 0xff, 20
+#endif
+
+#define DEFINE_LAYER_LIGHTS(...)                                               \
+  const uint8_t layer_colors[][3] = __VA_ARGS__;                               \
+  void keyboard_post_init_user(void) { layer_state_set_user(layer_state); }    \
+  layer_state_t layer_state_set_user(layer_state_t state) {                    \
+    uint8_t layer = get_highest_layer(state);                                  \
+    if (layer >= (sizeof(layer_colors) / sizeof(layer_colors[0]))) {           \
+      rgblight_sethsv(DefaultLayerColorHSV);                                   \
+    } else {                                                                   \
+      const uint8_t *color = layer_colors[layer];                              \
+      rgblight_sethsv(color[0], color[1], color[2]);                           \
+    }                                                                          \
+    return state;                                                              \
+  }
--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,4 +1,5 @@
 #include "keycodes.h"
+#include "keymap_us.h"
 #include <stdint.h>
 #include QMK_KEYBOARD_H
 
@@ -7,9 +8,14 @@ enum Layers {
   NumbersLayer,
   SystemLayer,
   ControlLayer,
-  // This stays last
-  MAX_LAYER_INDEX,
 };
+
+DEFINE_LAYER_LIGHTS({
+    [BaseLayer] = {DefaultLayerColorHSV},
+    [NumbersLayer] = {0x64, 0xff, 20},
+    [SystemLayer] = {0x00, 0xff, 20},
+    [ControlLayer] = {0xdd, 0xff, 20},
+})
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -18,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXX,                     LT(ControlLayer, KC_Q), KC_W, KC_E, KC_R, KC_T,
     LT(SystemLayer, KC_TAB), KC_A,                   KC_S, KC_D, KC_F, KC_G,
     XXX,                     KC_Z,                   KC_X, KC_C, KC_V, KC_B, KC_SLASH,
-    KC_DOT, /* gap */ MT(LGUI(MOD_LSFT), KC_BACKSLASH), MT(MOD_LCTL, KC_ENTER), MT(MOD_LGUI, KC_ESC), MOD_LSFT,
+    KC_DOT, /* gap */ MT(LGUI(MOD_LSFT), KC_BACKSLASH), MT(MOD_LCTL, KC_SPACE), MT(MOD_LGUI, KC_ESC), MT(MOD_LSFT, KC_LEFT_BRACKET),
 
          KC_UNDERSCORE, KC_Y, KC_U, KC_I, KC_O, KC_P,
          KC_TAB,        KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON,
@@ -27,12 +33,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   // Numbers layer
   [NumbersLayer] = LAYOUT(
-    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,
+    _______, KC_1, KC_2, KC_3, KC_4, KC_5,
     _______, _______,   _______, _______, _______, _______,
     _______, _______,   _______, _______, _______, _______, _______,
     _______, /* gap */  _______, _______, _______, _______,
 
-             _______, KC_7, KC_8, KC_9, _______, _______,
+             KC_6,    KC_7, KC_8, KC_9, _______, _______,
              _______, KC_4, KC_5, KC_6, _______, _______,
     _______, _______, KC_1, KC_2, KC_3, _______, _______,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /* gap */ XXXXXXX
@@ -41,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [SystemLayer] = LAYOUT(
     _______, _______,   _______, _______, _______, _______,
     _______, _______,   _______, _______, _______, _______,
-    _______, _______,   _______, _______, _______, _______, _______,
-    _______, /* gap */  _______, _______, _______, _______,
+    _______, QK_UNDERGLOW_VALUE_DOWN, QK_UNDERGLOW_VALUE_UP, _______, _______, _______, _______,
+    _______, /* gap */  QK_BOOTLOADER, _______, _______, _______,
 
              _______, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
              _______, KC_F4, KC_F5, KC_F6, KC_F12, _______,
@@ -73,25 +79,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //   _______, _______, _______, _______, /* gap */ _______
   // ),
 // clang-format on
-
-#define DefaultLayerColor RGB_WHITE
-
-const uint8_t PROGMEM layer_colors[][3] = {
-    [BaseLayer] = {0x00, 0x70, 0x70},
-    [NumbersLayer] = {0x00, 0xe9, 0x51},
-    [SystemLayer] = {0xFF, 0x00, 0x00},
-    [ControlLayer] = {DefaultLayerColor},
-};
-
-void keyboard_post_init_user(void) { rgblight_setrgb(DefaultLayerColor); }
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-  uint8_t layer = get_highest_layer(state);
-  if (layer >= MAX_LAYER_INDEX) {
-    rgblight_setrgb(RGB_WHITE);
-  } else {
-    const uint8_t *color = layer_colors[layer];
-    rgblight_setrgb(color[0], color[1], color[2]);
-  }
-  return state;
-}
